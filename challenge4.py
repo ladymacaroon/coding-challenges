8aba5563a66d0821acc686d1bbb643a2cf1cbd89
Ella Bowker <67073210+ladymacaroon@users.noreply.github.com>
2025-02-23 15:14:43 +0000

    Challenge5

--- challenge4.py
+++ challenge4.py
@@ -8,17 +8,10 @@ for caseNum in range(cases):
     line = sys.stdin.readline().rstrip()
 
     words = line.split('|')
-    anagram = words[1]
-    word = words[0]
-    correct = True
+    anagram = sorted(words[1])
+    word = sorted(words[0])
 
-    for x in range(len(anagram)):
-        if anagram[x].find(word[x]) != -1:
-            correct = True
-        else:
-            correct = False
-
-    if correct == True:
-        print(f'{line} =ANAGRAM')
+    if anagram == word:
+        print(f'{line} = ANAGRAM')
     else:
-        print(f'{line} =NOT AN ANAGRAM')
+        print(f'{line} = NOT AN ANAGRAM')
--- challenge5.py
+++ challenge5.py
@@ -7,4 +7,9 @@ cases = int(sys.stdin.readline().rstrip())
 for caseNum in range(cases):
     line = sys.stdin.readline().rstrip()
 
-    
+    animals = line.split(' ')
+    turkeys = int(animals[0]) *  2
+    goats = int(animals[1]) * 4
+    horses = int(animals[2]) * 4
+
+    print(turkeys + goats + horses)
