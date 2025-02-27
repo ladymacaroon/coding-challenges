e7f7d8d72447db82e48822c79f02a5e2694148ea
Ella Bowker <67073210+ladymacaroon@users.noreply.github.com>
2025-02-27 19:03:48 +0000

    completed challenge17!

--- challenge17.py
+++ challenge17.py
@@ -15,6 +15,16 @@ def compare_ships(ship1, ship2):
             return 1
         else:
             return 0
+        
+def advance_ships(ships):
+    for x in range(len(ships)):
+        ship = ships[x] 
+        match ship[3]:
+            case 'A': ship[0] -= 10
+            case 'B': ship[0] -= 20
+            case 'C': ship[0] -= 30
+            case _: print("error")
+        ships[x] = ship
 
 sections = int(sys.stdin.readline().rstrip())
 
@@ -26,8 +36,16 @@ for section in range(sections):
         line = sys.stdin.readline().rstrip()
 
         classes = line.split(':')
-        ship_type = classes[0]
+        ship_type = list(classes[0].split('_'))
         coordinates = list(map(int, classes[1].split(',')))
+        coordinates.append(ship_type[0])
+        coordinates.append(ship_type[1])
         all_coordinates.append(coordinates)
 
-    sorted_coordinates = sorted(all_coordinates, key=cmp_to_key(compare_ships))
+    while len(all_coordinates) > 0:
+        all_coordinates = sorted(all_coordinates, key=cmp_to_key(compare_ships))
+        destroyed_ship = all_coordinates.pop(0)
+        print(f"Destroyed Ship: {destroyed_ship[2]} xLoc {destroyed_ship[0]}")
+        advance_ships(all_coordinates)
+    
+    
